dd4b61bbe0492f6b48993d18f3b20dfaa40ab82d
Lúcio BJ <lbjunq@hotmail.com>
2022-02-09 17:08:36 -0300

    Complemento do nono requisito

--- services/post/postById.js
+++ services/post/postById.js
@@ -1,4 +1,6 @@
 const { BlogPost } = require('../../models');
+const { notFound } = require('../../utils/dictionary/statusCode');
+const errorConstructor = require('../../utils/functions/errorConstructor');
 
 const postsById = async (id) => {
   const selectedPost = await BlogPost.findByPk({
@@ -7,6 +9,9 @@ const postsById = async (id) => {
       { model: 'User', as: 'user', through: { attributes: [] } },
       { model: 'Category', as: 'categories', through: { attributes: [] } }],
     });
+  if (!selectedPost) {
+    throw errorConstructor(notFound, 'Post does not exist');
+  }
   return selectedPost;
 };
 
