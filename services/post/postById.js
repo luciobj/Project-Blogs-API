375821606dbe52324e48bf34bce8df190561bae8
Lúcio BJ <lbjunq@hotmail.com>
2022-02-12 19:23:29 -0300

    Completado o nono requisito

--- services/post/postById.js
+++ services/post/postById.js
@@ -1,14 +1,22 @@
-const { BlogPost } = require('../../models');
+const { BlogPost, User, Category } = require('../../models');
 const { notFound } = require('../../utils/dictionary/statusCode');
 const errorConstructor = require('../../utils/functions/errorConstructor');
 
 const postsById = async (id) => {
-  const selectedPost = await BlogPost.findByPk({
+  const selectedPost = await BlogPost.findOne({
     where: { id },
     include: [
-      { model: 'User', as: 'user', through: { attributes: [] } },
-      { model: 'Category', as: 'categories', through: { attributes: [] } }],
-    });
+      { model: User,
+        as: 'user',
+        attributes: { exclude: ['password'] },
+      },
+      { model: Category,
+        as: 'categories',
+        through: { attributes: [] },
+      },
+    ],
+    attributes: { exclude: ['UserId'] },
+  });
   if (!selectedPost) {
     throw errorConstructor(notFound, 'Post does not exist');
   }
